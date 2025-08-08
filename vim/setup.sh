f57749489abe08f3bb6e9376b39881eb91e08f40
Jankatay <162386918+Jankatay@users.noreply.github.com>
2025-08-08 14:11:43 +0000

    Update setup.sh

--- vim/setup.sh
+++ vim/setup.sh
@@ -36,7 +36,7 @@ fi
 # Haskell
 if [[ $(yes_or_no "Install ghcup and hls? (haskell tool installer and haskell autocorrect)") -eq 0 ]]; then
   curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-  bash -c ghcup install hls
+  source ~/.basrhc
   echo
 fi
 
